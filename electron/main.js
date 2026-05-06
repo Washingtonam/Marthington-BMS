bf4df6d9ff0e9e6a88ff60a9fe429091eac76f86
Washington Amedu <washingtonamedu@gmail.com>
2026-05-06 20:02:36 +0100

    first commit

--- electron/main.js
+++ electron/main.js
@@ -1,23 +1,35 @@
-const { app, BrowserWindow } = require("electron");
+const {
+  app,
+  BrowserWindow
+} = require("electron");
 
 const path = require("path");
 
+const isDev =
+  !app.isPackaged;
+
 function createWindow() {
 
-  const win = new BrowserWindow({
-    width: 1400,
-    height: 900,
+  const win =
+    new BrowserWindow({
 
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
+      width: 1400,
+      height: 900,
+
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true
+      }
+    });
 
   // DEV MODE
-  if (!app.isPackaged) {
+  if (isDev) {
 
-    win.loadURL("http://localhost:5173");
+    win.loadURL(
+      "http://localhost:5173"
+    );
+
+    win.webContents.openDevTools();
 
   }
 
@@ -25,11 +37,32 @@ function createWindow() {
   else {
 
     win.loadFile(
-      path.join(__dirname, "../dist/index.html")
+      path.join(
+        __dirname,
+        "../frontend/dist/index.html"
+      )
     );
   }
 }
 
 app.whenReady().then(() => {
+
   createWindow();
+
+  app.on("activate", () => {
+
+    if (
+      BrowserWindow.getAllWindows()
+        .length === 0
+    ) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
